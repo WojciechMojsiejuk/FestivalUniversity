77aa34882688e49204c6e14adc73c4a7413dfc59
WojciechMojsiejuk <wojciechmojsiejuk@gmail.com>
2021-09-15 10:23:41 +0300

    added spinning disco ball

---
+++ TheFestivalUniversity/Assets/Scripts/SpinningDiscoBall.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinningDiscoBall : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    Vector3 rotation = new Vector3(1, 0, 0);
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(rotation);
+    }
+}
