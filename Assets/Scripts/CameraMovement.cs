8567ab9ff7a6fe3f6c0cc6ece90e9839d0bf3a1f
WojciechMojsiejuk <wojciechmojsiejuk@gmail.com>
2021-09-16 16:32:49 +0300

    add zoomout & rotation

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -13,16 +13,28 @@ public class CameraMovement : MonoBehaviour
 
     private float yaw = 0.0f;
     private float pitch = 0.0f;
+    [SerializeField]
+    private Vector3 newposition = new Vector3(0, 0, -2);
+    [SerializeField]
+    private float zoomOutTime = 60f;
 
+    [SerializeField]
+    private float angle = 180f;
+    [SerializeField]
+    private float rotationTime = 60f;
+    private Quaternion desiredRotQ;
+
+    [SerializeField]
+    private float rotateAngle = 180f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //DiscoRotation();
+        StartCoroutine(SmoothTransition(transform.position, newposition, zoomOutTime));
     }
 
-    // Update is called once per frame
-    void Update()
+    void Manual()
     {
         yaw += speedH * Input.GetAxis("Mouse X");
         pitch -= speedV * Input.GetAxis("Mouse Y");
@@ -34,4 +46,58 @@ public class CameraMovement : MonoBehaviour
             transform.position = transform.position + Camera.main.transform.forward * distance * Time.deltaTime;
         }
     }
+
+
+    IEnumerator SmoothTransition(Vector3 startPosition, Vector3 endPosition, float seconds)
+    {
+        float t = 0f;
+        while (t <= 1f)
+        {
+            t += Time.deltaTime / seconds;
+            transform.position = Vector3.Lerp(startPosition, endPosition, Mathf.SmoothStep(0f, 1f, t));
+            yield return null;
+        }
+        DiscoRotation();
+    }
+
+    void DiscoRotation()
+    {
+        InvokeRepeating("InfinityAndBeyond", 0, rotationTime);
+
+
+    }
+
+    void InfinityAndBeyond()
+    {
+        
+        var x = Random.Range(-1*rotateAngle, rotateAngle);
+        var y = Random.Range(-1*rotateAngle, rotateAngle);
+        var z = Random.Range(-1 * rotateAngle, rotateAngle);
+        //generate angle
+        desiredRotQ = Quaternion.Euler(x, y, z);
+        StartCoroutine(RotateObject(rotationTime, desiredRotQ));
+    }
+
+    IEnumerator RotateObject(float time, Quaternion targetRotation)
+    {
+        float tParam = 0.0f;
+
+        Quaternion currentRotation = transform.rotation;
+
+        // Compares angles of two Quaternions
+        while (Quaternion.Angle(transform.rotation, targetRotation) > 0.1f)
+        {
+
+            tParam += Time.deltaTime * (1 / time);
+
+            transform.rotation = Quaternion.Slerp(currentRotation, targetRotation, tParam);
+
+            yield return null;
+        }
+
+        // Hard lock the rotation
+        transform.rotation = targetRotation;
+
+    }
+
 }
